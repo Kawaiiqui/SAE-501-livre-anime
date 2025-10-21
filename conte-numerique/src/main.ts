3465425ab7594c8ab1ca8d4f14f35e6a6bc3d7c6
Kawaiiqui <christellegineston@gmail.com>
2025-10-21 15:04:57 +0200

    node_modules_finally

--- conte-numerique/src/main.ts
+++ conte-numerique/src/main.ts
@@ -1,5 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModule } from './app/app-module';
 
-bootstrapApplication(AppComponent)
-  .catch((err: any) => console.error(err));
+platformBrowser().bootstrapModule(AppModule, {
+  ngZoneEventCoalescing: true,
+})
+  .catch(err => console.error(err));
